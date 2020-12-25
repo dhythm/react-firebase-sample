57fc759e5d3f31bf52300dd678633c4b79394c29
dhythm <yuta.okada.20@gmail.com>
2020-12-25 18:37:39 +0900

    feat(page): Get data from firestore db

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,3 +1,19 @@
+import { db } from '@app/lib/firebase-config';
 import React from 'react';
+import { useAsync } from 'react-use';
 
-export const HomePage: React.FunctionComponent = () => <div>Hello, world</div>;
+export const HomePage: React.FunctionComponent = () => {
+  const { loading, error, value: snapshot } = useAsync(
+    async () => await db.collection('users').get(),
+  );
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>Error</div>;
+
+  return (
+    <>
+      {snapshot.docs.map((doc, i) => (
+        <div key={i}>Hello, {doc.data().name}</div>
+      ))}
+    </>
+  );
+};
