1fb6cc73324a244c53b272fde254083cd1e4c483
dhythm <yuta.okada.20@gmail.com>
2020-12-26 18:08:14 +0900

    feat(functions): update teh calculation of compound interest

--- functions/src/apis/calculateCompoundInterest.ts
+++ functions/src/apis/calculateCompoundInterest.ts
@@ -1,6 +1,8 @@
 import { Decimal } from 'decimal.js';
 import { https, Response } from 'firebase-functions';
 
+Decimal.set({ rounding: 2 })
+
 export const calculateCompoundInterestApi = async (
   req: https.Request,
   res: Response<any>,
@@ -12,10 +14,14 @@ export const calculateCompoundInterestApi = async (
     if (isNaN(principal) || isNaN(interest) || isNaN(investmentPeriod)) {
       throw new Error('[Bad request] Invalid parameters');
     }
-    const assets = new Decimal(principal).times(
-      Decimal.pow(Decimal.add(1, Decimal.div(interest, 100)), investmentPeriod),
-    );
-    res.send({ assets });
+    const yearlyAssets = [...Array(Number(investmentPeriod)).keys()].reduce((acc: Decimal[], curr) => {
+      if (curr === 0) return [new Decimal(principal), new Decimal(principal).times(Decimal.add(1, Decimal.div(interest, 100)))]
+      return [...acc, new Decimal(acc[curr]).times(Decimal.add(1, Decimal.div(interest, 100)))]
+    }, [])
+    // const assets = new Decimal(principal).times(
+    //   Decimal.pow(Decimal.add(1, Decimal.div(interest, 100)), investmentPeriod),
+    // );
+    res.send({ yearlyAssets });
   } catch (e) {
     res.status(400).send('Error');
   }
